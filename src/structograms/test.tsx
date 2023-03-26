a251e0db75daf508566c91c6df474f6ed54db328
Euryn <crossdeltavi@gmail.com>
2023-03-26 19:25:50 +0200

    added `test.tsx` file

--- src/structograms/test.tsx
+++ src/structograms/test.tsx
@@ -11,22 +11,43 @@ import {
 export const Test: FC = () => {
   return (
     <Fragment>
-      <ProcessBlock>Test</ProcessBlock>
-      <ProcessBlock>Read n</ProcessBlock>
-
-      <SwitchBlock condition="rem of n / 3">
-        <CaseBlock condition="is 0">
-          <ProcessBlock>Hi</ProcessBlock>
-        </CaseBlock>
-        {/* <CaseBlock condition="is 1">
-          <ProcessBlock>:|</ProcessBlock>
-        </CaseBlock> */}
-        <CaseBlock condition="is 2">
-          <ProcessBlock>:(</ProcessBlock>
-        </CaseBlock>
-      </SwitchBlock>
-
-      <ProcessBlock>Exit</ProcessBlock>
+      {/* <ProcessBlock>In: N</ProcessBlock>
+      <ProcessBlock>In: AQ[1..N, 1..2]</ProcessBlock>
+      <ProcessBlock>result := 0</ProcessBlock>
+      <LoopBlock variant="first" condition="i = 2..N">
+        <SwitchBlock condition="cmp(i)">
+          <CaseBlock condition="True">
+            <ProcessBlock>result := result + 1</ProcessBlock>
+          </CaseBlock>
+          <CaseBlock condition="False">
+            <ProcessBlock>-</ProcessBlock>
+          </CaseBlock>
+        </SwitchBlock>
+      </LoopBlock>
+      <ProcessBlock>return result</ProcessBlock> */}
+      <ProcessBlock>In: N</ProcessBlock>
+      <ProcessBlock>In: AQ[1..N, 1..2]</ProcessBlock>
+      <ProcessBlock>result := 0</ProcessBlock>
+      <LoopBlock variant="first" condition="i = 2..N">
+        <ProcessBlock>
+          prev_morning := AQ[i - 1, 1],
+          <br />
+          prev_evening := AQ[i - 1, 2],
+          <br />
+          curr_morning := AQ[i, 1],
+          <br />
+          curr_evening := AQ[i, 2]
+        </ProcessBlock>
+        <SwitchBlock condition="curr_morning > prev_morning AND curr_evening > prev_evening">
+          <CaseBlock condition="True">
+            <ProcessBlock>result := result + 1</ProcessBlock>
+          </CaseBlock>
+          <CaseBlock condition="False">
+            <ProcessBlock>-</ProcessBlock>
+          </CaseBlock>
+        </SwitchBlock>
+      </LoopBlock>
+      <ProcessBlock>return result</ProcessBlock>
     </Fragment>
   );
 };
